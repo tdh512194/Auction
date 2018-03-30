22ea63e4f70c921cd6f03b687594b6c6112223bc
tdh512194 <tdh512194@gmail.com>
2018-03-30 10:08:57 +0700

    Revert "update"
    This reverts commit 0910d8569a903a34801c22f639bbb550e8db4321.

--- Auction2.py
+++ Auction2.py
@@ -63,11 +63,11 @@ class User(db.Model):
 
     def bid(self, item_id, price):
         """Bid action"""
-        bid = Bid.query.filter_by(item_id = item_id).filter_by(user_id = self.id).first() #select the corresponding row entry, select composite Pkey with 'and_'
+        bid = Bid.query.filter_by(item_id = item_id).filter_by(user_id = self.id).first() #select the corresponding row entry
         aucprice = Item.query.get(item_id).price
         if price >= aucprice: # bid price must be higher than auction price
             if bid is None: # user has NOT bidded on the item
-                bid = Bid(self.id, item_id, price) # create bid session in DB
+                bid = Bid(Bid.bidID, self.id, item_id, price) # create bid session in DB
                 db.session.add(bid)
                 db.session.commit()
                 print("User ", self.username,
@@ -87,18 +87,23 @@ class User(db.Model):
 
 class Bid(db.Model):
     __tablename__ = 'bid'
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    item_id = Column(Integer, ForeignKey('item.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('item.id'), nullable=False)
     price = Column(Float, nullable=False)
 
-    def __init__(self, user_id, item_id, price):
+    bidID = 1 # bidID starts from 1
+    def __init__(self, id, user_id, item_id, price):
+        self.id = id
         self.user_id = user_id
         self.item_id = item_id
         self.price = price
+        Bid.bidID += 1 # auto increment bidID each time someone bids
+
 
 u1 = User(1, 'USERNAME1', 'PASS1')
 u2 = User(2, 'USERNAME2', 'PASS2')
-u3 = User(6, 'USERNAME6', 'PASS6')
+u3 = User(3, 'USERNAME3', 'PASS3')
 
 @app.route('/')
 def hello_world():
@@ -106,11 +111,11 @@ def hello_world():
     db.create_all() # create the tables
 
     add_user() # add the created users to the DB
-
+    print('********Auction Session********')
     user_auction()
-
+    print('********Bid Session********')
     user_bid()
-
+    print('*******Querry Session*********')
     #query item ID
     query(1)
     query(10)
@@ -132,19 +137,23 @@ def user_auction():
 
 def user_bid():
     """itemID, bidPrice"""
+    print('Bid on item 1')
     u2.bid(1, 400)
     u3.bid(1, 200)
     u3.bid(1, 1000)
 
+    print('Bid on item 10')
     u1.bid(10, 2100)
     u3.bid(10, 3000)
     u1.bid(10, 2500)
 
+    print('Bid on item 3')
     u2.bid(3, 5500)
     u3.bid(3, 5600)
     u2.bid(3, 7000)
 
 def query(item_id):
+    item_name = Item.query.get(item_id).name # get by primary key
     query_result = Bid.query.filter_by(item_id = item_id).all()
     max = 0;
     for bid in query_result:
@@ -152,7 +161,9 @@ def query(item_id):
             max = bid.price
             name = User.query.get(bid.user_id).username # get by primary key
 
-    print(name, " has the highest bid of ", max)
+    print(name,
+          " has the highest bid of ", max,
+          " on item ", item_name)
 
 if __name__ == '__main__':
     app.run(debug = True)
